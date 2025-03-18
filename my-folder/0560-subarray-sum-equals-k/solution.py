a22b17d8892a2c97434873056ffb68c801ffc4ea
Srushti Pillare <158192885+Srushti-17@users.noreply.github.com>
2025-03-18 19:35:14 +0000

    [LeetCode Sync] Runtime - 46 ms (11.03%), Memory - 22.1 MB (26.02%)

---
+++ my-folder/0560-subarray-sum-equals-k/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums) # size of the given array.
+        mpp = defaultdict(int)
+        preSum = 0
+        cnt = 0
+
+        mpp[0] = 1 # Setting 0 in the map.
+        for i in range(n):
+            # add current element to prefix Sum:
+            preSum += nums[i]
+
+            # Calculate x-k:
+            remove = preSum - k
+
+            # Add the number of subarrays to be removed:
+            cnt += mpp[remove]
+
+            # Update the count of prefix sum
+            # in the map.
+            mpp[preSum] += 1
+
+        return cnt
