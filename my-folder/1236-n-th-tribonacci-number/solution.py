fd4827def5563104616bf5c7fe4b004099ffac2c
Srushti Pillare <158192885+Srushti-17@users.noreply.github.com>
2025-08-28 09:05:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

---
+++ my-folder/1236-n-th-tribonacci-number/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        arr = [0,1,1]
+
+        for i in range(0,n-2):
+            arr.append(arr[i] + arr[i+1] + arr[i+2])
+
+        return arr[n]
