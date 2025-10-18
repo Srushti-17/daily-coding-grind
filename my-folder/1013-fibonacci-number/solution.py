52d5f41eddcbca050bc3b327aaa33cb29ce210bb
Srushti Pillare <158192885+Srushti-17@users.noreply.github.com>
2025-10-18 09:30:58 +0000

    [LeetCode Sync] Runtime - 364 ms (5.10%), Memory - 17.9 MB (100.00%)

--- my-folder/1013-fibonacci-number/solution.py
+++ my-folder/1013-fibonacci-number/solution.py
@@ -1,16 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
-        first = 0
-        sec = 1
-
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        
-        for i in range(1,n):
-            third = first + sec
-            first = sec
-            sec = third
-
-        return third    
+        if n <= 1:
+            return n
+        else:
+            return self.fib(n - 1) + self.fib(n - 2)  
